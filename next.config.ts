9d4c689be841ce38aaa9b2e057cb69079fa76d72
Ajeet Kumar <87787220+ajeetkbhardwaj@users.noreply.github.com>
2026-08-21 20:43:49 +0530

    github pages configs for the github actions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
-import type { NextConfig } from "next";
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
+const repoName = 'mathcarecenter';
 
-const nextConfig: NextConfig = {
-  output: "standalone",
+const nextConfig = {
+  // If building on GitHub Actions for GitHub Pages, set the basePath
+  basePath: isGithubActions ? `/${repoName}` : '',
+  assetPrefix: isGithubActions ? `/${repoName}/` : '',
+  trailingSlash: true,
+  images: {
+    unoptimized: true, // required for static export on GitHub Pages
+  },
 };
 
 export default nextConfig;
